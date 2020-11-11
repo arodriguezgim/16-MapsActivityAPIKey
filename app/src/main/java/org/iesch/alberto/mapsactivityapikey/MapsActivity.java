5f336f5c145912d343510e64ad028b1bf326c595
Alberto <arodriguezg@iesch.org>
2020-11-11 15:46:45 +0100

    claseMiercoles

--- app/src/main/java/org/iesch/alberto/mapsactivityapikey/MapsActivity.java
+++ app/src/main/java/org/iesch/alberto/mapsactivityapikey/MapsActivity.java
@@ -2,13 +2,17 @@ package org.iesch.alberto.mapsactivityapikey;
 
 import androidx.fragment.app.FragmentActivity;
 
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
@@ -27,17 +31,39 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        try {
+            // Customise the styling of the base map using a JSON object defined
+            // in a raw resource file.
+            boolean success = googleMap.setMapStyle(
+                    MapStyleOptions.loadRawResourceStyle(
+                            this, R.raw.style_json));
+
+            if (!success) {
+                Log.e("TAG", "Style parsing failed.");
+            }
+        } catch (Resources.NotFoundException e) {
+            Log.e("TAG", "Can't find style. Error: ", e);
+        }
+
+
+
         mMap = googleMap;
 
-        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
 
         // Add a marker in Sydney and move the camera
         // LatLng sydney = new LatLng(-34, 151);
         LatLng teruel = new LatLng(40.3456, -1.10646);
-        LatLng taller2 = new LatLng(40.347646, -1.106432);
+        LatLng elajo = new LatLng(40.347646, -1.106432);
 
-        mMap.addMarker(new MarkerOptions().position(teruel).title("Taller San Miguel"));
-        mMap.addMarker(new MarkerOptions().position(taller2).title("Taller Alternativo"));
+        mMap.addMarker(
+                new MarkerOptions()
+                        .position(elajo)
+                        .title("PEÑA EL AJO")
+                        .snippet("A.C Peña el AJo - Concierto de la pegatina")
+                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN))
+
+        );
         mMap.moveCamera(CameraUpdateFactory.newLatLng(teruel));
     }
 }
